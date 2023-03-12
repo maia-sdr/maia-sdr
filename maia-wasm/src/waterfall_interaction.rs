maia-sdr/maia-sdr

commit 60db8ffcfd704e37346911bca543e7a22eb34c32

maia-wasm: refactor to improve waterfall reusability
Inspired by an experiment by bastibl about using the maia-wasm
waterfall in FutureSDR, this improves the reusability of the
waterfall by reorganizing the code of the start function by
maia-wasm into separate functions, so that these can be called
in other use cases. An example of a standalone waterfall will
show how to use these effectively.

The canvas resizing functions have been moved from Ui to
WaterfallInteraction in order to support use cases in which Ui is
not present, because the forms and REST API are missing. Moreover,
now WaterfallInteraction sets up its own callbacks on construction,
in the same way as Ui.

The crate console_error_panic_hook has been added to aid in debugging.

The wasm init function only registers the panic hook. Afterwards it
is necessary to call maia_wasm_start() or another function to initialize
the desired application.

Signed-off-by: Daniel Estévez <daniel@destevez.net>

maia-wasm/src/waterfall_interaction.rs
@@ -11,7 +11,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{HtmlCanvasElement, PointerEvent, WheelEvent};
+use web_sys::{HtmlCanvasElement, PointerEvent, WheelEvent, Window};
 
 /// Waterfall interaction controller.
 ///
@@ -23,11 +23,12 @@ use web_sys::{HtmlCanvasElement, PointerEvent, WheelEvent};
 ///   on the waterfall.
 #[derive(Clone)]
 pub struct WaterfallInteraction {
-    render_engine: Rc<RefCell<RenderEngine>>,
+    window: Rc<Window>,
     canvas: Rc<HtmlCanvasElement>,
-    pointer_tracker: Rc<RefCell<PointerTracker>>,
+    render_engine: Rc<RefCell<RenderEngine>>,
     waterfall: Rc<RefCell<Waterfall>>,
-    ui: Ui,
+    pointer_tracker: Rc<RefCell<PointerTracker>>,
+    ui: Option<Ui>,
     center_freq_overflow: Rc<RefCell<f32>>,
 }
 
@@ -36,35 +37,48 @@ impl WaterfallInteraction {
     ///
     /// The controller needs access to the [`RenderEngine`], in order to convert
     /// from pixels to units, to the [`Waterfall`] and its associated canvas
-    /// element, and to the [`Ui`] (which is used when the RX frequency needs to
-    /// be updated because of waterfall dragging).
-    ///
-    /// After this function returns, it is necessary to call
-    /// [`WaterfallInteraction::set_callbacks`] to create and register the
-    /// required event callbacks.
+    /// element.
     pub fn new(
-        render_engine: Rc<RefCell<RenderEngine>>,
+        window: Rc<Window>,
         canvas: Rc<HtmlCanvasElement>,
-        ui: Ui,
+        render_engine: Rc<RefCell<RenderEngine>>,
         waterfall: Rc<RefCell<Waterfall>>,
-    ) -> WaterfallInteraction {
-        WaterfallInteraction {
-            render_engine,
+    ) -> Result<WaterfallInteraction, JsValue> {
+        // Set the canvas default cursor to crosshair. This is overridden by
+        // some of the methods below.
+        canvas.style().set_property("cursor", "crosshair")?;
+
+        let interaction = WaterfallInteraction {
+            window,
             canvas,
-            pointer_tracker: Rc::new(RefCell::new(PointerTracker::new())),
+            render_engine,
             waterfall,
-            ui,
+            pointer_tracker: Rc::new(RefCell::new(PointerTracker::new())),
+            ui: None,
             center_freq_overflow: Rc::new(RefCell::new(0.0)),
-        }
+        };
+        interaction.set_callbacks();
+        Ok(interaction)
     }
 
-    /// Sets the callbacks required by the interaction controller.
+    /// Sets the [`Ui`] object associated to the waterfall.
     ///
-    /// This registers callbacks for the on wheel and on pointer
-    /// up/down/cancel/leave/move events of the waterfall canvas.
-    pub fn set_callbacks(&self) {
+    /// This object is used to update the RX frequency when the waterfall is
+    /// dragged much futher than its edge. If this function is not called,
+    /// updating the frequency when the waterfall is dragged will not be
+    /// supported (this is for the intended use case in which a `Ui` object is
+    /// not available).
+    pub fn set_ui(&mut self, ui: Ui) {
+        self.ui = Some(ui);
+    }
+
+    fn set_callbacks(&self) {
         // We leak all the closures produced by self to prevent them from being
         // dropped immediately.
+        self.resize_canvas()();
+        self.window
+            .set_onresize(Some(self.onresize().into_js_value().unchecked_ref()));
+
         self.canvas
             .set_onwheel(Some(self.onwheel().into_js_value().unchecked_ref()));
 
@@ -82,6 +96,20 @@ impl WaterfallInteraction {
 
         self.canvas
             .set_onpointermove(Some(self.onpointermove().into_js_value().unchecked_ref()));
+    }
+
+    fn resize_canvas(&self) -> impl Fn() {
+        let render_engine = Rc::clone(&self.render_engine);
+        let waterfall = Rc::clone(&self.waterfall);
+        move || {
+            let mut engine = render_engine.borrow_mut();
+            engine.resize_canvas().unwrap();
+            waterfall.borrow_mut().resize_canvas(&mut engine).unwrap();
+        }
+    }
+
+    fn onresize(&self) -> Closure<dyn Fn()> {
+        Closure::new(self.resize_canvas())
     }
 
     fn clamp_zoom(zoom: f32) -> f32 {
@@ -186,15 +214,16 @@ impl WaterfallInteraction {
                 let mut overflow = self.center_freq_overflow.borrow_mut();
                 *overflow += freq - clamped;
                 let shift_threshold = 0.25;
-                if overflow.abs() >= shift_threshold {
-                    // Change receive frequency
-                    let shift = shift_threshold.copysign(*overflow);
-                    *overflow -= shift;
-                    let (fc, fs) = waterfall.get_freq_samprate();
-                    let new_fc = fc + 0.5 * f64::from(shift) * fs;
-                    self.ui.set_rx_lo_frequency(new_fc as u64)?;
-                } else {
-                    waterfall.set_center_frequency(clamped);
+                match self.ui.as_ref() {
+                    Some(ui) if overflow.abs() >= shift_threshold => {
+                        // Change receive frequency
+                        let shift = shift_threshold.copysign(*overflow);
+                        *overflow -= shift;
+                        let (fc, fs) = waterfall.get_freq_samprate();
+                        let new_fc = fc + 0.5 * f64::from(shift) * fs;
+                        ui.set_rx_lo_frequency(new_fc as u64)?;
+                    }
+                    _ => waterfall.set_center_frequency(clamped),
                 }
             }
             PointerGesture::Pinch { center, dilation } => Self::apply_dilation(

maia-wasm/Cargo.toml
@@ -11,9 +11,10 @@ keywords = ["SDR", "FPGA", "wasm", "WebGL2"]
 categories = ["science", "webassembly", "web-programming::http-client", "web-programming::websocket"]
 
 [lib]
-crate-type = ["cdylib"]
+crate-type = ["cdylib", "lib"]
 
 [dependencies]
+console_error_panic_hook = "0.1"
 js-sys = "0.3"
 maia-json = { path = "../maia-httpd/maia-json", version = "0.1.0" }
 paste = "1.0"

maia-wasm/assets/index.js
@@ -1,7 +1,8 @@
-import init from "./pkg/maia_wasm.js";
+import init, { maia_wasm_start } from "./pkg/maia_wasm.js";
 
 async function run() {
-    const wasm = await init();
+    await init();
+    maia_wasm_start();
 };
 
 run();

maia-wasm/src/lib.rs
@@ -6,8 +6,8 @@
 
 use std::cell::RefCell;
 use std::rc::Rc;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::{Document, HtmlCanvasElement, Window};
 
 use crate::render::RenderEngine;
 use crate::ui::Ui;
@@ -24,32 +24,35 @@ pub mod waterfall;
 pub mod waterfall_interaction;
 pub mod websocket;
 
-/// Starts the web application.
+/// Initialize the wasm module.
 ///
-/// This is the main web application function. It is called when the web page is
-/// loaded, and it set up all the objects and callbacks that keep the web
-/// application running.
+/// This function is set to run as soon as the wasm module is instantiated. It
+/// applies some settings that are needed for all kinds of usage of
+/// `maia-wasm`. For instance, it sets a panic hook using the
+/// [`console_error_panic_hook`] crate.
 #[wasm_bindgen(start)]
-pub fn main() -> Result<(), JsValue> {
-    let window = Rc::new(web_sys::window().ok_or("unable to get window")?);
-    let document = Rc::new(window.document().ok_or("unable to get document")?);
+pub fn start() -> Result<(), JsValue> {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    Ok(())
+}
+
+/// Starts the maia-wasm web application.
+///
+/// This function starts the maia-wasm application. It should be called from
+/// JavaScript when the web page is loaded. It sets up all the objects and
+/// callbacks that keep the application running.
+#[wasm_bindgen]
+pub fn maia_wasm_start() -> Result<(), JsValue> {
+    let (window, document) = get_window_and_document()?;
     let canvas = Rc::new(
         document
             .get_element_by_id("canvas")
             .ok_or("unable to get #canvas element")?
             .dyn_into::<web_sys::HtmlCanvasElement>()?,
     );
-    canvas.style().set_property("cursor", "crosshair")?;
 
-    let render_engine = Rc::new(RefCell::new(RenderEngine::new(
-        Rc::clone(&canvas),
-        Rc::clone(&window),
-        &document,
-    )?));
-    let waterfall = Rc::new(RefCell::new(Waterfall::new(
-        &mut render_engine.borrow_mut(),
-        window.performance().ok_or("unable to get performance")?,
-    )?));
+    let (render_engine, waterfall, mut waterfall_interaction) =
+        new_waterfall(&window, &document, &canvas)?;
     WebSocketClient::start(&window, Rc::clone(&waterfall))?;
     let ui = Ui::new(
         Rc::clone(&window),
@@ -57,10 +60,68 @@ pub fn main() -> Result<(), JsValue> {
         Rc::clone(&render_engine),
         Rc::clone(&waterfall),
     )?;
-    let waterfall_interaction =
-        WaterfallInteraction::new(Rc::clone(&render_engine), canvas, ui, Rc::clone(&waterfall));
-    waterfall_interaction.set_callbacks();
+    waterfall_interaction.set_ui(ui);
 
+    setup_render_loop(render_engine, waterfall);
+
+    Ok(())
+}
+
+/// Returns the [`Window`] and [`Document`] objects.
+///
+/// These are returned inside an [`Rc`] so that their ownership can be shared.
+pub fn get_window_and_document() -> Result<(Rc<Window>, Rc<Document>), JsValue> {
+    let window = Rc::new(web_sys::window().ok_or("unable to get window")?);
+    let document = Rc::new(window.document().ok_or("unable to get document")?);
+    Ok((window, document))
+}
+
+/// Creates a [`Waterfall`] and associated objects.
+///
+/// This function creates a waterfall, the associated WebGL2 [`RenderEngine`],
+/// and the [`WaterfallInteraction`] object, which is used to control the
+/// waterfall based on user input.
+#[allow(clippy::type_complexity)]
+pub fn new_waterfall(
+    window: &Rc<Window>,
+    document: &Document,
+    canvas: &Rc<HtmlCanvasElement>,
+) -> Result<
+    (
+        Rc<RefCell<RenderEngine>>,
+        Rc<RefCell<Waterfall>>,
+        WaterfallInteraction,
+    ),
+    JsValue,
+> {
+    let render_engine = Rc::new(RefCell::new(RenderEngine::new(
+        Rc::clone(canvas),
+        Rc::clone(window),
+        document,
+    )?));
+    let waterfall = Rc::new(RefCell::new(Waterfall::new(
+        &mut render_engine.borrow_mut(),
+        window.performance().ok_or("unable to get performance")?,
+    )?));
+    let waterfall_interaction = WaterfallInteraction::new(
+        Rc::clone(window),
+        Rc::clone(canvas),
+        Rc::clone(&render_engine),
+        Rc::clone(&waterfall),
+    )?;
+    Ok((render_engine, waterfall, waterfall_interaction))
+}
+
+/// Sets up a render loop for the waterfall.
+///
+/// This function sets up a render loop using `requestAnimationFrame()`. Each
+/// time the the callback triggers, the waterfall is prepared for rendering and
+/// the render engine is called. Then, the rendering of the next frame is
+/// scheduled using `requestAnimationFrame()`.
+pub fn setup_render_loop(
+    render_engine: Rc<RefCell<RenderEngine>>,
+    waterfall: Rc<RefCell<Waterfall>>,
+) {
     let f = Rc::new(RefCell::new(None));
     let g = f.clone();
     *g.borrow_mut() = Some(Closure::new(move |dt| {
@@ -81,8 +142,6 @@ pub fn main() -> Result<(), JsValue> {
     }));
     // Initial requestAnimationFrame callback.
     request_animation_frame(g.borrow().as_ref().unwrap());
-
-    Ok(())
 }
 
 fn request_animation_frame(f: &Closure<dyn FnMut(f32)>) {

maia-wasm/src/ui.rs
@@ -98,9 +98,6 @@ impl Ui {
     }
 
     fn set_callbacks(&self) -> Result<(), JsValue> {
-        self.resize_canvas()();
-        self.window
-            .set_onresize(Some(self.onresize().into_js_value().unchecked_ref()));
         self.set_api_get_periodic(1000)?;
 
         set_on!(
@@ -186,20 +183,6 @@ impl Ui {
         self.window
             .set_interval_with_callback_and_timeout_and_arguments_0(handler, interval_ms)?;
         Ok(())
-    }
-
-    fn resize_canvas(&self) -> impl Fn() {
-        let render_engine = Rc::clone(&self.render_engine);
-        let waterfall = Rc::clone(&self.waterfall);
-        move || {
-            let mut engine = render_engine.borrow_mut();
-            engine.resize_canvas().unwrap();
-            waterfall.borrow_mut().resize_canvas(&mut engine).unwrap();
-        }
-    }
-
-    fn onresize(&self) -> Closure<dyn Fn()> {
-        Closure::new(self.resize_canvas())
     }
 
     fn colormap_select_onchange(&self) -> Closure<dyn Fn()> {

commit 289238b4ef378f767918e8afd243d1f01477cc75

maia-wasm: fix frequency change by waterfall scroll
60db8ffcfd704e37346911bca543e7a22eb34c32 broke the RX LO frequency change
by scrolling the waterfall. The reason for this is that by the time that
the onpointermove callback was created in WaterfallInteraction, self.ui
had not yet been set, so the callback would always see this as None, even
if we would update it later on.

The type of WaterfallInteraction::ui is now changed to an Rc<RefCell<>>,
so that the callback "sees" the change that happens later on, when
WaterfallInteraction::set_ui is called.

Signed-off-by: Daniel Estévez <daniel@destevez.net>

maia-wasm/src/waterfall_interaction.rs
@@ -28,7 +28,7 @@ pub struct WaterfallInteraction {
     render_engine: Rc<RefCell<RenderEngine>>,
     waterfall: Rc<RefCell<Waterfall>>,
     pointer_tracker: Rc<RefCell<PointerTracker>>,
-    ui: Option<Ui>,
+    ui: Rc<RefCell<Option<Ui>>>,
     center_freq_overflow: Rc<RefCell<f32>>,
 }
 
@@ -54,7 +54,7 @@ impl WaterfallInteraction {
             render_engine,
             waterfall,
             pointer_tracker: Rc::new(RefCell::new(PointerTracker::new())),
-            ui: None,
+            ui: Rc::new(RefCell::new(None)),
             center_freq_overflow: Rc::new(RefCell::new(0.0)),
         };
         interaction.set_callbacks();
@@ -69,7 +69,7 @@ impl WaterfallInteraction {
     /// supported (this is for the intended use case in which a `Ui` object is
     /// not available).
     pub fn set_ui(&mut self, ui: Ui) {
-        self.ui = Some(ui);
+        self.ui.borrow_mut().replace(ui);
     }
 
     fn set_callbacks(&self) {
@@ -221,7 +221,7 @@ impl WaterfallInteraction {
                 let mut overflow = self.center_freq_overflow.borrow_mut();
                 *overflow += freq - clamped;
                 let shift_threshold = 0.25;
-                match self.ui.as_ref() {
+                match self.ui.borrow().as_ref() {
                     Some(ui) if overflow.abs() >= shift_threshold => {
                         // Change receive frequency
                         let shift = shift_threshold.copysign(*overflow);